d5de4d58cd832e9a1a1bb25edd8bd65611b2b40f
Daniel Da Silva <100931944+projdanielsilva@users.noreply.github.com>
2023-04-01 12:43:56 -0300

    28/03

---
+++ projeto_pessoa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Pessoa p1 = new Pessoa();
+       //p1.InsereNome("ANA CLARA");
+       //p1.InsereSobreNome("CORDEIRO");
+       //p1.InsereIdade(15);
+
+       p1.MostraDados();  
+    }
+}

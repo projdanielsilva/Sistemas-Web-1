cbc1ee7d69f233ff448501bfa548ed466aea5bb6
Daniel Da Silva <100931944+projdanielsilva@users.noreply.github.com>
2023-04-25 10:30:19 -0300

    18/04

---
+++ Exercicio_18_04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio_18_04;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto Banco DANIEL");
+        Banco b = new Banco();
+        //c.Saldo = 1000;
+        b.Depositar(1000);
+        b.Sacar(1600);
+        //  com = é Set
+        // sem igual Get
+        b.AjustarLimite(500);
+
+        double valortotal = b.MostrarSaldo();
+
+        //Console.WriteLine("Seu saldo é de " + c.Saldo + " Com limite de " + c.Limite);
+
+        Console.WriteLine("O seu saldo total é de " + valortotal);
+        //Console.WriteLine("Seu saldo total é de " + c.Saldo);
+
+        b.imprimi(b.emprestimo(12,1000));
+    }
+}

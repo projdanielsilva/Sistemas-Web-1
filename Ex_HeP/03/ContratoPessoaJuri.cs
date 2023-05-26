eaa0e7fa14bacb4fa60c6f931343bc3c4dfbe16b
Daniel Da Silva <100931944+projdanielsilva@users.noreply.github.com>
2023-05-26 17:56:34 -0300

    Update ContratoPessoaJuri.cs

--- Ex_HeP/03/ContratoPessoaJuri.cs
+++ Ex_HeP/03/ContratoPessoaJuri.cs
@@ -2,19 +2,8 @@ namespace _03;
 
 class ContratoPessoaJuri:Contrato
 {
-public string? Cnpj {get;set;}
-public string? IE {get;set;}
-public string? NomeEmpresa {get;set;}
 
-//----------------------------------------------------//
-
-public override string MostraDados()
-{
-    return base.MostraDados()+" ---- "+this.Cnpj+ " ---- " + this.IE+ " ---- " + this.NomeEmpresa;
-}
-
-public override double CalcularPrestacao(double valor, int prazo)
-{
-  return base.CalcularPrestacao(valor, prazo) + 3;
-}
+public override double CalcularPrestacao(){
+    return ((base.CalcularPrestacao()/Prazo)+3);
+   } 
 }
